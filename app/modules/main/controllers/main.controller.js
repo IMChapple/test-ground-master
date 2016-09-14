27c69e44559ae52e452863c01e02d1fefd804aec
IChapple <Ian.Chapple@freightquote.com>
2016-09-14 17:00:41 -0500

    Interface changes to main ctrl
    -not impact related visual changes to main ctrl interface

--- app/modules/main/controllers/main.controller.js
+++ app/modules/main/controllers/main.controller.js
@@ -10,8 +10,17 @@
     MainCtrl.$inject = ['fqCancelableQ'];
     function MainCtrl(fqCancelableQ) {
 
-        var _request1   = new fqCancelableQ('https://public.opencpu.org/ocpu/library/?foo=' + 1);
-        var _request2   = new fqCancelableQ('https://public.opencpu.org/ocpu/library/?foo=' + 2);
+        /*
+            Eventually will use this controller to load in all of the internal components
+            each composing one of the features of the cancelableQ service (debounce, throttle, log, etc)
+
+            TODO: get a better test REST api
+        */
+
+        var _request1               = new fqCancelableQ('https://public.opencpu.org/ocpu/library/?foo=' + 1);
+        var _request2               = new fqCancelableQ('https://public.opencpu.org/ocpu/library/?foo=' + 2);
+        var _debRequestStandard     = new fqCancelableQ('https://public.opencpu.org/ocpu/library/?foo=' + 3, 500);
+        var _debRequestImmediate    = new fqCancelableQ('https://public.opencpu.org/ocpu/library/?foo=' + 4, 500, true);
 
         function _callFirstRequest() {
             _request1.get().then(function(res) {
@@ -42,13 +51,23 @@
             }
         }
 
+        function _debounceTest(immediate) {
+            var _test = immediate ? _debRequestImmediate : _debRequestStandard;
+            _test.get().then(function(res) {
+                console.log('success');
+            }, function (err) {
+                console.error(err);
+            });
+        }
+
         //extending what I want to expose onto 'this' rather than using vm. or ctrl. all over the place
         //this is simply easier to easier to read, and all in one location
         angular.extend(this, {
             request1: _request1,
             request2: _request2,
             request: _callFirstRequest,
-            addRequests: _addRequests
+            addRequests: _addRequests,
+            debounceTest: _debounceTest
         });
     }
 })();
